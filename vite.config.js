e54af5cd16709f4e065122343b85a2a85e011ac8
Uwich <ulrich.atienza@gmail.com>
2026-03-15 00:56:35 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/', // <- dynamic
+  base: "/PF_Q4_Portfolio_BSIT-32E2_Atienza_Ulrich/", 
 });
